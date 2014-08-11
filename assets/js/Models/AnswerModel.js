4cbf04c7b680aa2db5e3e288c44c8ede96b3904b
Jeffrey den Drijver <jeedendree@gmail.com>
2014-08-11 23:53:49 +0200

    Fix a minor answer submitting bug

--- assets/js/Models/AnswerModel.js
+++ assets/js/Models/AnswerModel.js
@@ -3,7 +3,7 @@ var AnswerModel = Backbone.Model.extend({
 	questionID: false,
 	
 	urlRoot: function(){
-		return Quest.url() + "/questions/" + this.get('questionID') + "/answer"
+		return Quest.url() + "/questions/" + this.get('questionID') + "/answer.json"
 	}
 
 });
